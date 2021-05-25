41276a07c83d504a168c5f51af524db97744ec31
klaybson <klaybson@gmail.com>
2021-05-24 21:45:31 -0300

    Aulas python

---
+++ execicio1.py
@@ -0,0 +1,11 @@
+
+nome = input('Escreva seu nome\n')
+cpf = input('Escreva seu CPF\n')
+endereco = input('Escreva seu end.:\n')
+idade = input('Escreva sua idade\n')
+altura = input('Escreva sua altura\n')
+telefone = input('Escreva sua tel.:\n')
+
+#Como comentar no python
+
+print('O seu nome é:',nome,'\nCPF:', cpf,'\nEnd.:', endereco ,'\nSua idade:',idade,'\nAltura:',altura, '\nTel.:',telefone )
