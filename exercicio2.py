93900bf17ce92860edd3a74588878bf4386dfb43
klaybson <klaybson@gmail.com>
2021-05-24 22:45:32 -0300

    Exercicio2

---
+++ exercicio2.py
@@ -0,0 +1,17 @@
+#Codigo Python execicio 2 da SOLYD cursos
+
+#Variavies
+idade = input('Digite sua idade:\n')
+peso = input('Digite seu peso:\n')
+
+#Converte o tipo da variavel
+idade = int(idade)
+peso = float(peso)
+
+#Estrutura de decisão
+if ((idade >= 18) and (peso >= 60)):
+    print("Vai pintar rua e cortar grama!")
+else:
+    print('Você é pior que 02!!')
+
+#Fim
