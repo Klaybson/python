24d80a3257545a0d1b8235307d9ce10c8f42b4f6
klaybson <klaybson@gmail.com>
2021-05-27 23:49:18 -0300

    Exercicio 4

--- exercicio3.py
+++ exercicio3.py
@@ -1,7 +1,7 @@
 cidades = ['salvador', 'sao bento', 'bomja', 1986]
 cidades.append(input('Acresente uma nova cidade:\n'))
 cidades.remove(1986)
-cidades.insert(0,'São Paulo')
+#cidades.insert(0,'São Paulo')
 
 print(cidades)
 print('A nova cidade é:',cidades[-1])
---
+++ exercicio4.py
@@ -0,0 +1,13 @@
+qt = input('Digite a quantidade de convidadeos\n')
+qt = int(qt)
+ps =[]
+i = 0
+j = 0
+
+while i < qt:
+    ps.append(input('Digite o nome do convidados:\n'))
+    i += 1
+
+while j < i :
+    print('Covidado: ',ps[-i])
+    i -= 1
